1d216facd1cb4d772a62cc67505377113f9106ab
All-with-Priyanshu <paintolapriyanshu1@gmail.com>
2025-08-06 17:42:30 +0530

    Add 5-Array.js

---
+++ 5-Array.js
@@ -0,0 +1,13 @@
+const numbers = [5, 7, 8, 9 ,2];
+
+let largest = numbers[0];
+let smallest = numbers[0];
+
+for (let i = 1 ; i>numbers.length ; i++){
+    if (number[i] > largest){
+        number[i] = largest;
+    }
+    else if (number[i] < smallest){
+        number[i] = smallest;
+    }
+}
