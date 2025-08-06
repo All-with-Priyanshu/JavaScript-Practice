2f68af914075f1d798a32a4e2424a6ea4cd479b4
All-with-Priyanshu <paintolapriyanshu1@gmail.com>
2025-08-06 12:09:15 +0530

    Hello

---
+++ 1-Datatype.js
@@ -0,0 +1,13 @@
+// Create a program to check the datatype //
+let a = "Hello World";
+let b = "Hello123" ;
+let c = 1234;
+let d = 1.0;
+let e = "abcd@1234";
+let f = true;
+console.log(typeof a); //string//
+console.log(typeof b);//string//
+console.log(typeof c);//number//
+console.log(typeof d);//number//
+console.log(typeof e);//string//
+console.log(typeof f);//boolean//
