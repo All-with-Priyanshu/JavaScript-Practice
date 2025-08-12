b65f7cbdac0d228cbd0f03926888d2d715449648
All-with-Priyanshu <paintolapriyanshu1@gmail.com>
2025-08-12 16:49:02 +0530

    New files

---
+++ 8-If_Else.js
@@ -0,0 +1,11 @@
+let a = "hello";
+let b = 12;
+if (typeof a === String) {
+    console.log("Ye toh string hai");
+}
+else if(typeof b === Number){
+    console.log("Hmm, Ye toh int hai");
+}
+else{
+    console.log("Not Successful!");
+}
